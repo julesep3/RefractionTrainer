7ca38730c5ffb9a435d0088581e59de38f9a0210
julesep3 <50899932+julesep3@users.noreply.github.com>
2022-07-05 13:01:18 -0500

    add sign in module template

--- public/app.js
+++ public/app.js
@@ -1,15 +1,15 @@
-const buttonNumber = document.getElementById('button-number');
-const buttonName = document.getElementById('button-name');
-const rootTag = document.getElementById('root');
+// const buttonNumber = document.getElementById('button-number');
+// const buttonName = document.getElementById('button-name');
+// const rootTag = document.getElementById('root');
 
-buttonNumber.addEventListener('click', fNumber);
-buttonName.addEventListener('click', fName);
+// buttonNumber.addEventListener('click', fNumber);
+// buttonName.addEventListener('click', fName);
 
-function fNumber() {
-	console.log('number button is pressed');
-	rootTag.innerHTML = 'Number is 23434';
-}
-function fName() {
-	console.log('name button is pressed');
-	rootTag.innerHTML = 'Name is Blort!';
-}
+// function fNumber() {
+// 	console.log('number button is pressed');
+// 	rootTag.innerHTML = 'Number is 23434';
+// }
+// function fName() {
+// 	console.log('name button is pressed');
+// 	rootTag.innerHTML = 'Name is Blort!';
+// }
