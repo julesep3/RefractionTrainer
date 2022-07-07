1155b3b5353043dd6fbc4799da408789fc92dee2
julesep3 <50899932+julesep3@users.noreply.github.com>
2022-07-07 12:52:43 -0500

    add preventDefault() to signIn function

--- public/controller/firebase_auth.js
+++ public/controller/firebase_auth.js
@@ -3,6 +3,7 @@ import * as Elements from '../view/elements.js'
 export function addEventListeners() {
 
 	Elements.formSignIn.addEventListener('submit', e => {
+		e.preventDefault();
 		const email = e.target.email.value;
 		const password = e.target.password.value;
 	});
