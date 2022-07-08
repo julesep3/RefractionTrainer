e93d18c94af4050ebc4ae1a2a410f700f6e960ae
julesep3 <50899932+julesep3@users.noreply.github.com>
2022-07-08 12:17:20 -0500

    add genPurpose modalInfoBox template and element

--- public/view/elements.js
+++ public/view/elements.js
@@ -3,3 +3,10 @@ export const formSignIn = document.getElementById('form-signin');
 
 // buttons
 export const navbarSignOut = document.getElementById('navbar-signout');
+
+// modals
+export const modalInfoBox = {
+	modal: new bootstrap.Modal(document.getElementById('modal-infobox'), { backdrop: 'static' }),
+	title: document.getElementById('modal-infobox-title'),
+	body: document.getElementById('modal-infobox-body'),
+}
