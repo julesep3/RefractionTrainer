7cae397a49ea2145b71db5f0fedab8537a1b0005
julesep3 <50899932+julesep3@users.noreply.github.com>
2022-07-19 14:02:25 -0500

    add routing module

--- public/app.js
+++ public/app.js
@@ -1,7 +1,15 @@
 import * as FirebaseAuth from './controller/firebase_auth.js'
 import * as HomePage from './view/home_page.js'
 import * as AboutPage from './view/about_page.js'
+import { route } from './controller/routing.js';
 
 FirebaseAuth.addEventListeners();
 HomePage.addEventListeners();
 AboutPage.addEventListeners();
+
+window.onload = () => {
+	const pathname = window.location.pathname;
+	const hash = window.location.hash;
+
+	route(pathname, hash);
+}
---
+++ public/controller/routing.js
@@ -0,0 +1,21 @@
+import { home_page } from "../view/home_page.js;";
+import { about_page } from "../view/about_page.js";
+
+export const routePath = {
+	HOME: '/',
+	ABOUT: '/about',
+}
+
+export const routes = [
+	{ path: routePath.HOME, page: home_page },
+	{ path: routePath.ABOUT, page: about_page },
+];
+
+export function route(pathname, hash) {
+	// within 'routes', find the object where the object's path matches the pathname
+	const route = routes.find(element => element.path == pathname);
+	// if the route exists, call on that page's function
+	if (route) route.page();
+	// if route doesn't exist, call on default (home) page
+	else routes[0].page();
+}
